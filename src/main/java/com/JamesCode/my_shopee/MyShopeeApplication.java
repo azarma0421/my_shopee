a71c749e4acab4190f35dcce6a1837fea18ad5c6
James <super890421@gmail.com>
2023-05-07 22:05:51 +0800

    initialize

---
+++ src/main/java/com/JamesCode/my_shopee/MyShopeeApplication.java
@@ -0,0 +1,13 @@
+package com.JamesCode.my_shopee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyShopeeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyShopeeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/JamesCode/my_shopee/MyShopeeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.JamesCode.my_shopee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyShopeeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
