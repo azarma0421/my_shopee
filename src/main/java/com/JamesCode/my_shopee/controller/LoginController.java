f9ee4814896166130938ef5e291564eb255f9800
James <super890421@gmail.com>
2023-05-07 22:51:16 +0800

    show 'HOME'

---
+++ src/main/java/com/JamesCode/my_shopee/controller/LoginController.java
@@ -0,0 +1,14 @@
+package com.JamesCode.my_shopee.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LoginController {
+
+    @GetMapping("/")
+    public String showhome(){
+        return "HOME";
+    }
+}
