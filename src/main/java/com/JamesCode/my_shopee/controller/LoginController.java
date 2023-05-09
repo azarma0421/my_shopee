bd23840817d831e34003d885628d38109845d17d
James <super890421@gmail.com>
2023-05-09 22:35:23 +0800

    where's my navbar??

--- src/main/java/com/JamesCode/my_shopee/controller/LoginController.java
+++ src/main/java/com/JamesCode/my_shopee/controller/LoginController.java
@@ -1,14 +1,15 @@
 package com.JamesCode.my_shopee.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+
+@Controller
 public class LoginController {
 
     @GetMapping("/")
-    public String showhome(){
-        return "HOME";
+    public String showhome(Model model){
+        return "Home";
     }
 }
