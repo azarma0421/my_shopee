f771e26ac3f839142b1badc420bee2ccdef9bbb9
James <super890421@gmail.com>
2023-06-28 23:02:28 +0800

    refactor: use @Data in entity

--- src/main/java/com/JamesCode/my_shopee/entity/Product.java
+++ src/main/java/com/JamesCode/my_shopee/entity/Product.java
@@ -1,9 +1,11 @@
 package com.JamesCode.my_shopee.entity;
 
 import jakarta.persistence.*;
+import lombok.Data;
 
 @Entity
 @Table(name="Product")
+@Data
 public class Product {
 
     @Id
@@ -28,71 +30,4 @@ public class Product {
 
     @Column(name="src")
     private String src;
-
-    public Product() {
-    }
-    public Product(String category, String name, int cost, int num, String onsale, String src) {
-        this.category = category;
-        this.name = name;
-        this.cost = cost;
-        this.num = num;
-        this.onsale = onsale;
-        this.src = src;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getCategory() {
-        return category;
-    }
-
-    public void setCategory(String category) {
-        this.category = category;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public int getCost() {
-        return cost;
-    }
-
-    public void setCost(int cost) {
-        this.cost = cost;
-    }
-
-    public int getNum() {
-        return num;
-    }
-
-    public void setNum(int num) {
-        this.num = num;
-    }
-
-    public String getOnsale() {
-        return onsale;
-    }
-
-    public void setOnsale(String onsale) {
-        this.onsale = onsale;
-    }
-
-    public String getSrc() {
-        return src;
-    }
-
-    public void setSrc(String src) {
-        this.src = src;
-    }
 }
--- src/main/java/com/JamesCode/my_shopee/entity/User.java
+++ src/main/java/com/JamesCode/my_shopee/entity/User.java
@@ -1,8 +1,10 @@
 package com.JamesCode.my_shopee.entity;
 
 import jakarta.persistence.*;
+import lombok.Data;
 
 @Entity
+@Data
 @Table(name="user")
 public class User {
 
@@ -19,55 +21,4 @@ public class User {
 
     @Column(name="password")
     private String password;
-
-    public User() {
-
-    }
-    public User(String username, String mail, String password) {
-        this.username = username;
-        this.mail = mail;
-        this.password = password;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getMail() {
-        return mail;
-    }
-
-    public void setMail(String mail) {
-        this.mail = mail;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    @Override
-    public String toString() {
-        return "User{" +
-                "id=" + id +
-                ", username='" + username + '\'' +
-                ", mail='" + mail + '\'' +
-                ", password='" + password + '\'' +
-                '}';
-    }
 }
